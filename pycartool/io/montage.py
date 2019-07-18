add5584176cf729bc9c34d859e91f3a4af22285d
victor ferat <victor.ferat@live.fr>
2019-07-18 15:08:50 +0200

    Initial commit

---
+++ pycartool/io/montage.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Authors: Tanguy Vivier <tanguy.viv@gmail.com>
+#          Victor Férat <victor.ferat@live.fr>
+#
+# License: BSD (3-clause)
+
+from mne.channels import Montage
+import numpy as np
+
+def xyz_to_montage(path, kind=''):
+    """Reads and convert xyz positions to a mne montage type
+
+    Parameters
+    ----------
+    path : str
+        The filepath of the xyz file.
+
+    Returns
+    -------
+    montage : mne.channels.montage.Montage
+        Montage for EEG electrode locations.
+    """
+
+    n = int(open(path).readline().lstrip().split(' ')[0])
+    coord = np.loadtxt(path, skiprows=1, usecols=(0, 1, 2), max_rows=n)
+    names = np.loadtxt(path, skiprows=1, usecols=3, max_rows=n,
+                       dtype=np.dtype(str))
+    names = names.tolist()
+    montage = Montage(coord, names, kind,
+                   selection=[i for i in range(n)])
+    return(montage)
---
+++ pycartool/tests/test_montage.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Authors: Victor Férat <victor.ferat@live.fr>
+#
+# License: BSD (3-clause)
+
+import os
+import pytest
+
+from mne.channels.montage import Montage
+from ..io.montage import xyz_to_montage
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+data_path = os.path.join(dir_path, "data")
+
+def test_xyz_to_montage():
+    file_path = os.path.join(data_path, "EGI257.GenevaAverage13.10-10.xyz")
+    montage = xyz_to_montage(file_path)
+    print(type(montage))
+    assert type(montage) == Montage
