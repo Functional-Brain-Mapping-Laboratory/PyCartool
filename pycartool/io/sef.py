119fcaa5f912e35b402912df23a62d8f96d1ad74
victor ferat <victor.ferat@live.fr>
2019-07-18 15:45:01 +0200

    ADD sef

---
+++ pycartool/io/sef.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+# Authors: Tanguy Vivier <tanguy.viv@gmail.com>
+#          Victor Férat <victor.ferat@live.fr>
+#
+# License: BSD (3-clause)
+import struct
+import re
+
+import numpy as np
+import mne
+from mne.io import RawArray
+from mne import create_info
+
+
+def read_sef(path):
+    """
+    Reads file with format .sef, and returns a mne.io.Raw object containing
+    the data.
+
+    Parameters
+    ----------
+    path : str
+        The filepath of the sef file.
+
+    Returns
+    -------
+    raw : mne.io.RawArray
+        RawArray containing the EEG signals.
+    """
+    f = open(path, 'rb')
+    #   Read fixed part of the headerà
+    version = f.read(4).decode('utf-8')
+    n_channels,         = struct.unpack('I', f.read(4))
+    num_aux_electrodes, = struct.unpack('I', f.read(4))
+    num_time_frames,    = struct.unpack('I', f.read(4))
+    sfreq,              = struct.unpack('f', f.read(4))
+    year,               = struct.unpack('H', f.read(2))
+    month,              = struct.unpack('H', f.read(2))
+    day,                = struct.unpack('H', f.read(2))
+    hour,               = struct.unpack('H', f.read(2))
+    minute,             = struct.unpack('H', f.read(2))
+    second,             = struct.unpack('H', f.read(2))
+    millisecond,        = struct.unpack('H', f.read(2))
+
+    #   Read variable part of the header
+    ch_names = []
+    for k in range(n_channels):
+        name = [char for char in f.read(8).split(b'\x00')
+                if char != b''][0]
+        ch_names.append(name.decode('utf-8'))
+
+    # Read data
+    buffer = np.frombuffer(
+        f.read(n_channels * num_time_frames * 8),
+        dtype=np.float32,
+        count=n_channels * num_time_frames)
+    data = np.reshape(buffer, (num_time_frames, n_channels))
+
+    ch_types = ['eeg' for i in range(n_channels)]
+    infos = create_info(
+        ch_names=ch_names, sfreq=sfreq,
+        ch_types=ch_types)
+    raw = RawArray(np.transpose(data), infos)
+    return (raw)
+
+
+def write_sef(path, raw):
+    """Export a raw mne file to a sef file.
+
+    Parameters
+    ----------
+    path : str
+        File name of the exported dataset.
+    raw : instance of mne.io.Raw
+        The raw data to export.
+    """
+    n_channels = len(raw.info['ch_names'])
+    num_freq_frames = raw.n_times
+    info = raw.info
+    sfreq = info['sfreq']
+    num_aux_electrodes = n_channels - len(mne.pick_types(info, meg=False,
+                                                         eeg=True,
+                                                         exclude=[""]))
+    f = open(path, 'wb')
+    f.write("SE01".encode('utf-8'))
+    f.write(struct.pack('I', n_channels))
+    f.write(struct.pack('I', num_aux_electrodes))
+    f.write(struct.pack('I', num_freq_frames))
+    f.write(struct.pack('f', sfreq))
+    f.write(struct.pack('H', 0))
+    f.write(struct.pack('H', 0))
+    f.write(struct.pack('H', 0))
+    f.write(struct.pack('H', 0))
+    f.write(struct.pack('H', 0))
+    f.write(struct.pack('H', 0))
+    f.write(struct.pack('H', 0))
+
+    ch_names = info["ch_names"]
+    for k in range(n_channels):
+        ch_name = ch_names[k]
+        ch_name = ch_name.ljust(8)
+        f.write(ch_name.encode('utf-8'))
+
+    data = raw.get_data().astype(np.float32)
+    data = np.reshape(data, n_channels * num_freq_frames, order='F')
+    data.tofile(f)
+    f.close()
---
+++ pycartool/tests/test_sef.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Authors: Victor Férat <victor.ferat@live.fr>
+#
+# License: BSD (3-clause)
+
+import os
+import pytest
+
+import mne
+from ..io.sef import read_sef, write_sef
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+data_path = os.path.join(dir_path, "data")
+
+
+def test_read_sef():
+    return()
+
+
+def test_write_sef():
+    return()
