7b871b4df26459c32f1e26d9b1d3d7c81bca1355
victor ferat <victor.ferat@live.fr>
2019-07-19 15:03:36 +0200

    ADD setup.py

---
+++ pycartool/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+with open("../README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='pycartool',
+
+     version='0.1',
+
+     scripts=['dokr'] ,
+
+     author="Victor Férat",
+
+     author_email="victor.ferat@live.fr",
+
+     description="A simple open source Python package for I/O between Cartool"
+                 " and MNE ( and more generally python)",
+
+     long_description=long_description,
+
+     long_description_content_type="text/markdown",
+
+     url="https://github.com/vferat/PyCartool",
+
+     packages=setuptools.find_packages(),
+
+     license="BSD-3-Clause",
+
+     python_requires='>=3.6',
+
+     install_requires=["mne", "numpy"],
+
+     packages=setuptools.find_packages(exclude=['docs', 'tests']),
+
+     classifiers=[
+
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+ )
