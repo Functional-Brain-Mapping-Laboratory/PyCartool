e2377388e5c215e9b95f4b42050399ddd81f5c65
Victor <victor.ferat@live.fr>
2019-12-05 10:44:46 +0100

    fix isscalar

--- pycartool/source_estimate.py
+++ pycartool/source_estimate.py
@@ -58,13 +58,17 @@ def read_ris(filename, source_space=None, subject=None):
         s_freq = struct.unpack('f', f.read(4))[0]
         print(f'Samplimg frequency: {s_freq}')
         isinversescalar = struct.unpack('c', f.read(1))[0]
-        if isinversescalar == b'0':
+        print(isinversescalar)
+        if isinversescalar == b'\x01':
             n_dim = 1
             print(f'Result of Inverse Solution computation is Scalar')
-        else:
+        elif isinversescalar == b'\x00':
             print(f'Result of Inverse Solution computation is Vectorial')
             n_dim = 3
-
+        else:
+            raise ValueError(f'isinversescalar must be either 1 for scalar, '
+                             f'either 0 for vectorial, but '
+                             f'{ord(isinversescalar)} found.')
         buf = f.read(n_dim * n_solutionpoints * n_timeframes * 4)
         data = np.frombuffer(buf, dtype=np.float32)
         data = data.reshape(n_timeframes, n_solutionpoints, n_dim)
@@ -93,7 +97,7 @@ def write_ris(source_estimate, filename):
     if data.ndim != 3:
         raise ValueError('Input data must be a 3D array')
     if data.shape[1] == 1:
-        isinversescalar = b'0'
+        isinversescalar = b'\x01'
     elif data.shape[1] == 3:
         isinversescalar = b'\x00'
     else:
