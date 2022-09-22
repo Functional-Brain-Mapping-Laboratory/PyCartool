3aabe69fd91af24613f157949ec71dd4df37c8fd
Victor Férat <victor.ferat@live.Fr>
2022-09-22 18:29:37 +0200

    Update inv.py

--- pycartool/inv/inv.py
+++ pycartool/inv/inv.py
@@ -27,7 +27,7 @@ def read_is(filename, verbose=None):
         Inverse solution matrices. n_dim=1 if solutions are scalar or
         n_dim=3 for vectorial solutions.
         shape(``n_regularizations``, ``n_dim``, ``n_solutionpoints``, ``n_channels``).
-    """ # noqa
+    """  # noqa
     with open(filename, "rb") as f:
         logger.info(f"Reading {filename}")
         logger.info(f"Reading Header...")
